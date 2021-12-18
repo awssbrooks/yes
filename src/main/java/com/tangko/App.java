3c44ce8b825eedd5d58bb9f80bf44a668366d703
EC2 Default User <ec2-user@ip-172-31-85-65.ec2.internal>
2021-12-18 12:18:10 +0000

    o

---
+++ src/main/java/com/tangko/App.java
@@ -0,0 +1,13 @@
+package com.tangko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
